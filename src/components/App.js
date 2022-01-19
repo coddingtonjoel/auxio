1aae08de1d8737e995fc7f541e8694bf916c07c2
jesster007 <jeston.bond@biola.edu>
2022-01-19 11:45:59 -0800

    Changing the main display to Welcome to Auxio!

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import '../App.css'
 function App() {
   return (
     <div>
-      <h1>Hello, Electron!</h1>
+      <h1>Welcome to Auxio!</h1>
 
       <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
     </div>
