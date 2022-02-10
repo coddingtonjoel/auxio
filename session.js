9b9322db3bf57de5f5f1775e23f45117ef71f44c
jesster007 <jeston.bond@biola.edu>
2022-02-09 16:08:03 -0800

    Added comments to session.js. Outlined future functions in that file.

--- session.js
+++ session.js
@@ -1,15 +1,26 @@
 function generateSesId(){
     let result = '';
-    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    for (let i = 0; i < 10; i++ ) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-        if(i == 2 || i == 5)
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'; //List of characters to choose from
+    for (let i = 0; i < 10; i++ ) { 
+        result += characters.charAt(Math.floor(Math.random() * characters.length)); //Add a random character to result
+        if(i == 2 || i == 5) //Add the - at indexes 3 and 6
             result += '-'
     }
     return result;
 }
 
 function joinSession(id){
+    
+   
+    //socket = Call firebase to get shared socket information and store it in socket
+    //while(socket == -1) 
+       // socket = Call firebase to get shared socket information and store it in socket
+    
+}
+
+function createSession(){
+    id = generateSesId();
+    //Send id to firebase
 
 }
 
