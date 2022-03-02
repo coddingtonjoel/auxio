95f645d560a5817baa6037f94f9c1f1fee5d5571
danieltmarch <43455439+danieltmarch@users.noreply.github.com>
2022-03-02 10:40:21 -0800

    Re-pushing changed in firebase.js

--- api/firebase.js
+++ api/firebase.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 const {initializeApp} = require("firebase/app");
 const {getAuth} = require("firebase/auth");
+const {getDatabase, ref, set, onValue, get} = require("firebase/database");
+const { WatchIgnorePlugin } = require("webpack");
+const {GoogleCred} = require("./google.js");
 //const { getAnalytics } = require("firebase/analytics");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,9 +11,12 @@ const {getAuth} = require("firebase/auth");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
+// Initialize Firebase
+
 const firebaseConfig = {
   apiKey: "AIzaSyCfjtyToI-PljpBTlvpUMq-HW9UZ5pR-As",
   authDomain: "auxio-26c70.firebaseapp.com",
+  databaseURL: "https://auxio-26c70-default-rtdb.firebaseio.com",
   projectId: "auxio-26c70",
   storageBucket: "auxio-26c70.appspot.com",
   messagingSenderId: "902655600558",
@@ -18,12 +24,46 @@ const firebaseConfig = {
   measurementId: "G-M2Y1KTGR5J"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const authentification = getAuth(app);
+class Database
+{
+
+  static app = null;
+  static authentification = null;
+  static userCredentials = null; //note .accessToken member
+  static db = null; //the real time firebase database object
+
+  static initServer()
+  {
+    Database.app = initializeApp(firebaseConfig);
+    Database.authentification = getAuth(Database.app);
+    Database.db = getDatabase(Database.app);
+  }
+
+  static requestCredentials = () => {
+    Database.userCredentials = GoogleCred.getCredentials();
+    //console.log(Database.userCredentials.accessToken);
+  }
+
+  //location and listener function needed, see AppMenu.js for example. listener function is called once the data is retrieved and whenever the data field is changed on the server.
+  static getData = (location, listenFunc) => {
+    onValue(ref(Database.db, location), listenFunc);
+    return -1;
+  }
+
+  //set, location, and data needed, see AppMenu.js for usage example
+  static createData = (location, data) => {
+    //console.log(data);
+    set(ref(Database.db, location), data);
+  }
+
+  //actually just a set data to null in the given location, this will remove the piece of data (and label).
+  static deleteData = (location) => {
+    return set(ref(Database.db, location), null);
+  }
+}
+
 //const analytics = getAnalytics(app);
 
 module.exports = {
-  app,
-  authentification
+  Database
 }
