beb803cda8a21ca4f6c514ec8fc8dc869a8bbb78
jesster007 <jeston.bond@biola.edu>
2022-03-11 22:28:23 -0800

    Changed the default createSession function to be an array

--- session.js
+++ session.js
@@ -37,6 +37,7 @@ class Session{
     }
 
     static queueSong(songId){
+        // Queue not currently working
         Session.queue.push(songId);
         Database.createData("Server/" + Session.sId, { "queue" : Session.queue});
     }
@@ -46,7 +47,7 @@ class Session{
         Database.initServer();
         Database.requestCredentials();
         //let tQueue = Session.queue;
-        Database.createData("Server/" + Session.sId, { "queue" : "test"});
+        Database.createData("Server/" + Session.sId, { "queue" : ["test2"]});
         //Database.createData("Server/" + Session.sId, { "queue" : ["test2"]});
         //Send id to firebase
 
