ab7eee9b64ffa294930ec4a44668a6c87437b170
Joel Coddington <coddingtonjoel@gmail.com>
2022-03-23 17:15:23 -0700

    Fixed session deletion issue

--- main.js
+++ main.js
@@ -221,10 +221,14 @@ app.on('ready', () => {
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  Session.deleteSession();
   if (process.platform !== 'darwin') {
     app.quit()
   }
+})
+
+app.on("before-quit", () => {
+  // if session host quits the app, end the session
+  Session.deleteSession();
 })
 
 app.on('activate', () => {
